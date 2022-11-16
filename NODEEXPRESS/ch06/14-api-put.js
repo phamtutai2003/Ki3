f567e4de864f287ee908a042611d7ac9b896758d
phạm tú tài <93572750+phamtutai1338@users.noreply.github.com>
2022-11-16 15:59:05 +0700

    16

---
+++ NODEEXPRESS/ch06/14-api-put.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+const tours =[
+ { id: 0, name: 'Hood River', price: 99.99},
+ { id: 1, name: 'Oregon Coast', price: 149.95},
+]
+
+app.get('/api/tours', (req,res) => {
+    const tourXml = '<?xml version="1.0"?><tours>' + 
+    tours.map(p => 
+        `<tour price="${p.price}" id= "${p.id}">${p.name}</tours>`
+        ).join('') + '</tours>'
+        const toursText = tours.map(p=>
+            `${p.id}: ${p.name} (${p.price})`
+             ).join('\n')
+             res.format({ 
+                'application/jon': () => res.json(tours),
+                'application/xml': () => res.type('application/xml').send(toursXml),
+                'text/xml': () => res.type('text/xml').send(toursXml),
+                'text/plain': () => res.type('text/plain').send(toursXml),
+
+             })
+})
+
+app.use('*', (req, res) => res.send('check out <a href="/api/tours">/api/tours</a.!'))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}\n`))
