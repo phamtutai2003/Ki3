cd1f06d60882ebbf475bd67d2ff91ae7ab43131e
phạm tú tài <93572750+phamtutai1338@users.noreply.github.com>
2023-03-06 19:43:23 +0700

    up

---
+++ Realtions portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Realtions_portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
